62c7949b18c501f8379697acd2791a63b81377ed
Elia <eliiia@protonmail.com>
2022-04-16 17:40:53 +0100

    body-parser middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 import { Express } from "express";
 
+import bodyParser from "body-parser";
+
 import logger from "./middleware/logger";
 
 export default (app: Express) => {
+    app.use(bodyParser.json());
+
     app.use(logger);
 };
