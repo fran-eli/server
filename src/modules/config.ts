5bf60f717cf1051d8f94cfab3df53f634a934772
Elia <eliiia@protonmail.com>
2022-04-21 21:17:55 +0100

    config.ts contains jwt_secret

--- src/modules/config.ts
+++ src/modules/config.ts
@@ -1,7 +1,13 @@
 import { config } from "dotenv";
 config();
 
+if (process.env.JWT_SECRET === undefined)
+    new Error("JWT_SECRET environment variable is undefined");
+
 export default {
     port: process.env.PORT || 3000,
     version: process.env.npm_package_version,
+    api: {
+        jwt_secret: process.env.JWT_SECRET as string,
+    },
 };
